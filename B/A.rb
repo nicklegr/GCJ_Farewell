9c7a5982dc709cc4416b8260717f4644c49c4d43
nicklegr <1327502+nicklegr@users.noreply.github.com>
2023-04-16 00:19:58 +0900

    B: A wip

---
+++ B/A.rb
@@ -0,0 +1,176 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    arg.each do |e|
+      PP.pp(e, STDERR)
+    end
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    STDERR.puts(*arg)
+  end
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+def puts_sync(str)
+  puts str
+  STDOUT.flush
+end
+
+def array_2d(r, c)
+  ret = []
+  r.times do
+    ret << [0] * c
+  end
+  ret
+end
+
+class Integer
+  def popcount32
+    bits = self
+    bits = (bits & 0x55555555) + (bits >>  1 & 0x55555555)
+    bits = (bits & 0x33333333) + (bits >>  2 & 0x33333333)
+    bits = (bits & 0x0f0f0f0f) + (bits >>  4 & 0x0f0f0f0f)
+    bits = (bits & 0x00ff00ff) + (bits >>  8 & 0x00ff00ff)
+    return (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff)
+  end
+
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
+
+# main
+t_start = Time.now
+
+cases = readline().to_i
+
+(1 .. cases).each do |case_index|
+  n = ri
+  as = ris
+  la, ra, lb, rb = ris
+
+  sum = 0
+  if ra < lb
+    # 111122211111111
+    # aaaaaa
+    #          bbbb
+    a = ra
+    b = lb
+ppd a, b
+    loop do
+      a += 1 if a < b
+      b -= 1 if a < b
+ppd a, b
+      break if a == b
+    end
+ppd a, b
+    sum = as[0..a-1].sum
+  elsif rb < la
+    # 111122211111111
+    #          aaaa
+    # bbbbbb
+    a = la
+    b = rb
+    loop do
+      a -= 1 if a < b
+      b += 1 if a < b
+      break if a == b
+    end
+    sum = as[a-1..-1].sum
+  elsif ra == lb
+    # 12345678
+    # aaaaa
+    #     bbbb
+    sum = as[0..ra-1].sum
+  elsif rb == la
+    # 12345678
+    #     aaaa
+    # bbbbb
+    sum = as[la-1..-1].sum
+  else
+    # 12345678
+    # aaaaa
+    #    bbbbb
+    # a4 -> b5
+    # a5 -> b4 or b6の和が多い側
+    for i in 0...n
+      sum_l = as[0..i].sum
+      sum_r = as[i...n].sum
+      sum_i = [sum_l, sum_r].min
+
+ppd "#{i}, #{sum_i}"
+      sum = [sum, sum_i].max
+    end
+  end
+
+  puts "Case ##{case_index}: #{sum}"
+end
+
+STDERR.puts("time: #{Time.now - t_start} s")
+
+__END__
+
+少しもかぶってない場合は一番bobに近い方から開始
+111122211111111
+aaaaaa
+         bbbb
+その後お互い距離を詰めるのが最適
+
+12345678
+aaaaa
+    bbbb
+a5,b6
+
+12345678
+aaaaa
+   bbbbb
+a4 -> b5
+a5 -> b4 or b6の和が多い側
