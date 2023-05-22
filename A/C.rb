0b3a9cf035b99af5a4b9d8f20ef9ddcbb2c7e010
nicklegr <1327502+nicklegr@users.noreply.github.com>
2023-05-22 13:47:54 +0900

    A: C-large OK

---
+++ A/C.rb
@@ -0,0 +1,121 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    arg.each do |e|
+      PP.pp(e, STDERR)
+    end
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    STDERR.puts(*arg)
+  end
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+def puts_sync(str)
+  puts str
+  STDOUT.flush
+end
+
+def array_2d(r, c)
+  ret = []
+  r.times do
+    ret << [0] * c
+  end
+  ret
+end
+
+class Integer
+  def popcount32
+    bits = self
+    bits = (bits & 0x55555555) + (bits >>  1 & 0x55555555)
+    bits = (bits & 0x33333333) + (bits >>  2 & 0x33333333)
+    bits = (bits & 0x0f0f0f0f) + (bits >>  4 & 0x0f0f0f0f)
+    bits = (bits & 0x00ff00ff) + (bits >>  8 & 0x00ff00ff)
+    return (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff)
+  end
+
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
+
+# main
+t_start = Time.now
+
+cases = readline().to_i
+
+(1 .. cases).each do |case_index|
+  n = ri
+  cards = ris
+
+  hash = {}
+  num = 0
+  ng = false
+  cards.each do |e|
+    if hash.key?(e)
+      written = hash[e]
+      if num > written
+        ng = true
+        break
+      end
+    else
+      num += 1
+      hash[e] = num
+    end
+  end
+
+  sorted = hash.to_a.sort_by{|e| e[1]}.map{|e| e[0]}
+  sorted.map!{|e| e.to_s}
+
+  puts "Case ##{case_index}: #{ng ? "IMPOSSIBLE" : sorted.join(" ")}"
+end
+
+STDERR.puts("time: #{Time.now - t_start} s")
